2cd58c767f7a7a3cb910b3ee64106f49b2f3f8e8
Unix&Me <unixnme@gmail.com>
2017-08-26 23:33:25 +0900

    softplus seems to work fine

--- trig.py
+++ trig.py
@@ -4,6 +4,8 @@ import numpy as np
 import keras
 import matplotlib.pyplot as plt
 import keras.backend as K
+import os
+
 
 np.random.seed(0)
 
@@ -12,10 +14,11 @@ FREQ = 1
 NUM_TRAIN_SAMPLES = int(NUM_SAMPLES * 0.75)
 
 batch_size = 10
-epochs = 50
+epochs = 10
+model_path = 'model.h5'
 
 def func(x):
-    return np.sin(x) ** 2 + np.random.randn(*x.shape)*0.1
+    return np.sin(x) ** 2 + np.random.randn(*x.shape)*0.01
 
 def relu():
     return keras.layers.Lambda(lambda x: K.maximum(0., x))
@@ -40,18 +43,23 @@ def create_network(hidden_units, activation):
     return model
 
 hidden_layers = [10,10]
-activations = [softplus(10.), relu()]
-labels = ['softplus10', 'relu']
+activations = ['relu', softplus(10.), softplus(5.), softplus(2.), softplus(1.)]
+labels = ['relu', 'softplus10', 'softplus5', 'softplus2', 'softplus1']
 fig1, ax1 = plt.subplots()
 fig2, ax2 = plt.subplots()
+if os.path.isfile(model_path):
+    os.remove(model_path)
 for activation, label in zip(activations, labels):
     np.random.seed(0)
     model = create_network(hidden_layers, activation)
+    if os.path.isfile(model_path):
+        model.load_weights(model_path)
     history = model.fit(verbose=False, x=x_train, y=y_train, batch_size=batch_size, validation_data=(x_test, y_test), epochs=epochs, shuffle=True)
 
     pred = model.predict(x_test)
     ax1.scatter(x_test, pred, label=label, marker='.')
     ax2.plot(history.history['loss'], label=label)
+    model.save_weights(model_path)
 
 ax1.scatter(x_test, y_test, label='true', marker='.')
 ax1.legend()
